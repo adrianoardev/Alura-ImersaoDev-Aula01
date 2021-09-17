3928a6688cd880df903cc23475129f23c8cdaf19
Adriano <adrianoardev@gmail.com>
2021-09-16 23:08:46 -0300

    Commit Aula 01 Imersão Dev - Variáveis, Operações e Média

---
+++ js.js
@@ -0,0 +1,15 @@
+var nome ="Adriano";
+var notaDoPrimeiroBimestre = 9.5;
+var notaDoSegundoBimestre = 7.4;
+var notaDoTerceiroBimestre = 4.5;
+var notaDoQuartoBimestre = 2.5;
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4;
+
+var notaFixada = notaFinal.toFixed(1);
+
+console.log(" Bem vindo " + nome);
+console.log("Sua Nota Média Final é: " + notaFixada);
+
+// Revisão
+// Variáveis, strings, console.log, toFixed, operações matemáticas, concatenação
